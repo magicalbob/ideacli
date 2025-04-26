0603bbb78c86ba32f69bd32a295f6250afdad1ee
Ian Ellis <ellisiana@gmail.com>
2025-04-27 00:05:08 +0100

    Added the show command

--- src/ideacli/cli.py
+++ src/ideacli/cli.py
@@ -6,6 +6,7 @@ import sys
 from ideacli.repository import init_repo, status
 from ideacli.add import add
 from ideacli.list import list_ideas
+from ideacli.show import show_idea
 
 def main():
     """Main entry point for the ideacli command."""
@@ -19,10 +20,20 @@ def main():
     # Status command
     status_parser = subparsers.add_parser("status", help="Check status of ideas repository")
     status_parser.add_argument("--path", help="Path to the repository")
+
+    # Add command
     add_parser = subparsers.add_parser("add", help="Add a new idea to the repository")
     add_parser.add_argument("--path", help="Path to the repository")
+
+    #List command
     list_parser = subparsers.add_parser("list", help="List all ideas")
+    list_parser.add_argument("--path", help="Path to the repository")
     
+    # Show command
+    show_parser = subparsers.add_parser("show", help="Show a specific idea by ID")
+    show_parser.add_argument("--path", help="Path to the repository")
+    show_parser.add_argument("--id", help="ID of the idea to show")
+
     args = parser.parse_args()
     
     if args.command == "init":
@@ -33,6 +44,8 @@ def main():
         add(args)
     elif args.command == "list":
         list_ideas(args)
+    elif args.command == "show":
+        show_idea(args)
     else:
         parser.print_help()
 
